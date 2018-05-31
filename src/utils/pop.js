1ce02d578c36765d6afe481fe6a98cf5bcc4f1b2
Artem Zakharchenko <kettanaito@gmail.com>
2018-05-31 13:04:43 +0200

    Adds "pop" util

---
+++ src/utils/pop.js
@@ -0,0 +1,3 @@
+export default function pop(arr) {
+  return arr.slice(0, arr.length - 1)
+}
---
+++ src/utils/pop.spec.js
@@ -0,0 +1,14 @@
+import { expect } from 'chai'
+import pop from './pop'
+
+test('Returns the copy of the array with the last entry removed', () => {
+  expect(pop([1, 2, 3])).to.deep.equal([1, 2])
+  expect(pop([1, 2])).to.deep.equal([1])
+  expect(pop([1])).to.deep.equal([])
+})
+
+test('Original array remains untouched', () => {
+  const arr = [1, 2]
+  pop(arr)
+  expect(arr).to.deep.equal([1, 2])
+})
