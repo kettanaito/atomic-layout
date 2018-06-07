1ec9474622cb6627645c623cac41c544716eaf66
Artem Zakharchenko <kettanaito@gmail.com>
2018-06-07 10:37:30 +0200

    parsePropName: Adds test case for custom breakpoint names parsing

--- src/utils/parsePropName.spec.js
+++ src/utils/parsePropName.spec.js
@@ -1,4 +1,5 @@
 import { expect } from 'chai'
+import Layout from '../Layout'
 import parsePropName from './parsePropName'
 
 test('Parses a prop name without breakpoint or behavior', () => {
@@ -31,4 +32,26 @@ test('Ignores unknown strings', () => {
   expect(res).to.have.property('purePropName', 'gutterFoo')
   expect(res).to.have.property('breakpointName', undefined)
   expect(res).to.have.property('behavior', 'up')
+})
+
+test('Parses a prop name with custom breakpoint name', () => {
+  Layout.configure({
+    breakpoints: {
+      mobile: {},
+      tablet: {},
+      desktop: {}
+    }
+  })
+
+  expect(parsePropName('gutterMobile')).to.have.property('purePropName', 'gutter')
+  expect(parsePropName('gutterMobile')).to.have.property('breakpointName', 'mobile')
+
+  expect(parsePropName('gutterTablet')).to.have.property('purePropName', 'gutter')
+  expect(parsePropName('gutterTablet')).to.have.property('breakpointName', 'tablet')
+
+  expect(parsePropName('gutterDesktop')).to.have.property('purePropName', 'gutter')
+  expect(parsePropName('gutterDesktop')).to.have.property('breakpointName', 'desktop')
+
+  expect(parsePropName('gutterFoo')).to.have.property('purePropName', 'gutterFoo')
+  expect(parsePropName('gutterFoo')).to.have.property('breakpointName', undefined)
 })
