c4268cce4096569143efb2f559cc0b48d0990825
Artem Zakharchenko <kettanaito@gmail.com>
2018-06-11 13:44:47 +0200

    Adds "cy.assert" custom command to await areas change properly

--- cypress/integration/behavior/inclusive.js
+++ cypress/integration/behavior/inclusive.js
@@ -1,26 +1,21 @@
 it('Inclusive', () => {
-  cy.visitExample('behavior/inclusive')
+  cy.visit('/behavior/inclusive')
 
   const assertAllVisible = () => {
-    cy.get('#first').should('be.visible')
-    cy.get('#second').should('be.visible')
-    cy.get('#third').should('be.visible')
+    cy.assert(['#first', '#second', '#third'], 'be.visible')
   }
 
-  cy.get('#first').should('be.visible')
-  cy.get('#second').should('be.visible')
-  cy.get('#third').should('not.be.visible')
+  cy.assert(['#first', '#second'], 'be.visible')
+  cy.assert(['#third'], 'not.be.visible')
 
   cy.setBreakpoint('sm').then(assertAllVisible)
   cy.setBreakpoint('md').then(assertAllVisible)
   cy.setBreakpoint('lg').then(() => {
-    cy.get('#first').should('be.visible')
-    cy.get('#second').should('be.visible')
-    cy.get('#third').should('not.be.visible')
+    cy.assert(['#first', '#second'], 'be.visible')
+    cy.assert(['#third'], 'not.be.visible')
   })
   cy.setBreakpoint('xl').then(() => {
-    cy.get('#first').should('be.visible')
-    cy.get('#second').should('be.visible')
-    cy.get('#third').should('not.be.visible')
+    cy.assert(['#first', '#second'], 'be.visible')
+    cy.assert(['#third'], 'not.be.visible')
   })
 })
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,1 +1,4 @@
-module.exports = () => {}
+module.exports = (on, config) => {
+  // `on` is used to hook into various events Cypress emits
+  // `config` is the resolved Cypress config
+}
