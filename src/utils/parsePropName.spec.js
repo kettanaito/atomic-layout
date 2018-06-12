4dda128a16986630409216a597ed6cfa3a5a25d9
Artem Zakharchenko <kettanaito@gmail.com>
2018-06-12 15:25:50 +0200

    parsePropName: Supports mixed prop name scenarios

--- src/utils/parsePropName.spec.js
+++ src/utils/parsePropName.spec.js
@@ -2,6 +2,14 @@ import { expect } from 'chai'
 import Layout from '../Layout'
 import parsePropName from './parsePropName'
 
+test('Parses a prop name with a breakpoint and behavior', () => {
+  const res = parsePropName('gutterLgOnly')
+  expect(res).to.be.an('object')
+  expect(res).to.have.property('purePropName', 'gutter')
+  expect(res).to.have.property('breakpointName', 'lg')
+  expect(res).to.have.property('behavior', 'only')
+})
+
 test('Parses a prop name without breakpoint or behavior', () => {
   const res = parsePropName('gutter')
   expect(res).to.be.an('object')
@@ -10,20 +18,12 @@ test('Parses a prop name without breakpoint or behavior', () => {
   expect(res).to.have.property('behavior', 'up')
 })
 
-test('Parses a prop name with a breakpoint', () => {
+test('Parses a prop name with breakpoint', () => {
   const res = parsePropName('gutterMd')
   expect(res).to.be.an('object')
   expect(res).to.have.property('purePropName', 'gutter')
   expect(res).to.have.property('breakpointName', 'md')
   expect(res).to.have.property('behavior', 'up')
-})
-
-test('Parses a prop name with a breakpoint and behavior', () => {
-  const res = parsePropName('gutterLgOnly')
-  expect(res).to.be.an('object')
-  expect(res).to.have.property('purePropName', 'gutter')
-  expect(res).to.have.property('breakpointName', 'lg')
-  expect(res).to.have.property('behavior', 'only')
 })
 
 test('Ignores unknown strings', () => {
