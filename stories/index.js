a1b3758259fa674b8a27d84dcd627ac4bab2df33
Artem Zakharchenko <kettanaito@gmail.com>
2018-06-13 13:51:03 +0200

    Storybook: Includes behavior scenarios as stories

--- stories/index.js
+++ stories/index.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import './styles.css'
 
 import Playground from './Playground'
 import Demo from './Demo'
 
+/* Behavior */
+import MobileFirst from '../examples/behavior/MobileFirst'
+import Inclusive from '../examples/behavior/Inclusive'
+import Bell from '../examples/behavior/Bell'
+
 storiesOf('Stories', module)
   .add('Playground', () => <Playground />)
   .add('Demo', () => <Demo />)
+
+storiesOf('Behavior', module)
+  .add('Mobile first', () => <MobileFirst />)
+  .add('Inclusive', () => <Inclusive />)
+  .add('Bell', () => <Bell />)
