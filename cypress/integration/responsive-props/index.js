514ab0434d2c83d586ecb47cde9af38ec6c55b50
Artem Zakharchenko <kettanaito@gmail.com>
2018-06-15 09:28:16 +0200

    Adds cutom configuration test

--- cypress/integration/responsive-props/index.js
+++ cypress/integration/responsive-props/index.js
@@ -2,4 +2,5 @@ describe('Responsive props', () => {
   require('./mobile-first')
   require('./breakpoint-specific')
   require('./inclusive-bell')
+  require('./custom-configuration')
 })
---
+++ examples/responsive-props/CustomConfiguration.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import Layout, { Composition } from '@lib'
+import Square from '../../stories/Square'
+
+Layout.configure({
+  defaultUnit: 'rem',
+  breakpoints: {
+    mobile: {
+      maxWidth: '575px',
+    },
+    tablet: {
+      minWidth: '576px',
+      maxWidth: '768px',
+    },
+    desktop: {
+      minWidth: '769px',
+    },
+  },
+})
+
+const template = `
+  'first'
+  'second'
+`
+
+const CustomConfiguration = () => (
+  <Composition id="composition" template={template} padding={1}>
+    {({ First, Second }) => (
+      <React.Fragment>
+        <First>
+          <Square>First</Square>
+        </First>
+        <Second>
+          <Square>Second</Square>
+        </Second>
+      </React.Fragment>
+    )}
+  </Composition>
+)
+
+export default CustomConfiguration
