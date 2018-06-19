ba87f8122cc06d380ab90c25bc2c72902f2ec643
Artem Zakharchenko <kettanaito@gmail.com>
2018-06-19 14:30:30 +0200

    Adds "assertArea" to integration tests

--- cypress/integration/behavior/inclusive.js
+++ cypress/integration/behavior/inclusive.js
@@ -3,19 +3,37 @@ it('Inclusive', () => {
 
   const assertAllVisible = () => {
     cy.assert(['#first', '#second', '#third'], 'be.visible')
+    cy.assertArea('#first', 'first')
+    cy.assertArea('#second', 'second')
+    cy.assertArea('#third', 'third')
   }
 
   cy.assert(['#first', '#second'], 'be.visible')
+  cy.assertArea('#first', 'first')
+  cy.assertArea('#second', 'second')
   cy.assert(['#third'], 'not.be.visible')
 
+  /* Small */
   cy.setBreakpoint('sm').then(assertAllVisible)
+
+  /* Medium */
   cy.setBreakpoint('md').then(assertAllVisible)
+
+  /* Large */
   cy.setBreakpoint('lg').then(() => {
     cy.assert(['#first', '#second'], 'be.visible')
+    cy.assertArea('#first', 'first')
+    cy.assertArea('#second', 'second')
+
     cy.assert(['#third'], 'not.be.visible')
   })
+
+  /* Extra-large */
   cy.setBreakpoint('xl').then(() => {
     cy.assert(['#first', '#second'], 'be.visible')
+    cy.assertArea('#first', 'first')
+    cy.assertArea('#second', 'second')
+
     cy.assert(['#third'], 'not.be.visible')
   })
 })
