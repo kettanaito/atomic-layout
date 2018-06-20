e09775a019caf288729a4e9b433d239790d16954
Artem Zakharchenko <kettanaito@gmail.com>
2018-06-20 13:57:50 +0200

    Adds custom "notIntersectWith" command. Favors commands chaining

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,6 +24,7 @@
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
+import { assert } from 'chai'
 import defaultOptions from '../../src/const/defaultOptions'
 
 Cypress.Commands.add('setBreakpoint', (breakpointName) => {
@@ -32,12 +33,54 @@ Cypress.Commands.add('setBreakpoint', (breakpointName) => {
       ? defaultOptions.breakpoints[breakpointName]
       : breakpointName
 
-  return cy.viewport(breakpoint.minWidth || 550, breakpoint.minHeight || 550)
+  cy.viewport(breakpoint.minWidth || 550, breakpoint.minHeight || 550)
+  cy.wait(100)
 })
 
-Cypress.Commands.add('assertArea', (selector, gridArea) => {
-  cy.get(selector).should('have.css', 'grid-row-start', gridArea)
-  cy.get(selector).should('have.css', 'grid-row-end', gridArea)
-  cy.get(selector).should('have.css', 'grid-column-start', gridArea)
-  cy.get(selector).should('have.css', 'grid-column-end', gridArea)
+Cypress.Commands.add('haveArea', { prevSubject: true }, (subject, gridArea) => {
+  cy.wrap(subject).should('have.css', 'grid-row-start', gridArea)
+  cy.wrap(subject).should('have.css', 'grid-row-end', gridArea)
+  cy.wrap(subject).should('have.css', 'grid-column-start', gridArea)
+  return cy.wrap(subject).should('have.css', 'grid-column-end', gridArea)
 })
+
+Cypress.Commands.add(
+  'haveSameAxis',
+  { prevSubject: true },
+  (subject, target, axis) => {
+    const rectA = subject[0].getBoundingClientRect()
+    cy.get(target).then((elem) => {
+      const rectB = elem[0].getBoundingClientRect()
+
+      expect(rectA[axis]).to.equal(rectB[axis])
+
+      return subject
+    })
+  },
+)
+
+Cypress.Commands.add(
+  'notIntersectWith',
+  { prevSubject: true },
+  (subject, targetSelector) => {
+    const a = subject[0].getBoundingClientRect()
+    cy.get(targetSelector).then((elem) => {
+      const b = elem[0] && elem[0].getBoundingClientRect()
+
+      const intersectsByX = Math.max(
+        0,
+        Math.min(a.right, b.right) - Math.max(a.left, b.left),
+      )
+      const intersectsByY = Math.max(
+        0,
+        Math.min(a.bottom, b.bottom) - Math.max(a.top, b.top),
+      )
+
+      const intersectionArea = intersectsByX * intersectsByY
+
+      assert.isAtMost(intersectionArea, 0, 'Must not intersect')
+
+      return subject
+    })
+  },
+)
