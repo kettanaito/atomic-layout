7c767057bce053a2f3b1f010a7fd0691bf9a920f
Artem Zakharchenko <kettanaito@gmail.com>
2018-06-21 14:16:34 +0200

    getAreasList: Adds unit tests for behaviors "down" and "only"

--- src/utils/getAreasList/getAreasList.spec.js
+++ src/utils/getAreasList/getAreasList.spec.js
@@ -3,23 +3,87 @@ import Layout from '../../Layout'
 import getAreasList from './getAreasList'
 
 test('Parses template props properly', () => {
-  const list = getAreasList({
+  const areasList = getAreasList({
+    foo: 'bar',
+    template: `a b`,
+  })
+
+  expect(areasList).to.deep.equal({
+    areas: ['a', 'b'],
+    templates: [
+      {
+        areas: ['a', 'b'],
+        behavior: 'up',
+        breakpoint: Layout.getBreakpoint('xs'),
+      },
+    ],
+  })
+})
+
+test('Returns proper areas list for "behavior:up"', () => {
+  const areasList = getAreasList({
     template: `a b`,
     templateMd: `a b c`,
   })
 
-  expect(list).to.deep.equal({
+  expect(areasList).to.deep.equal({
     areas: ['a', 'b', 'c'],
     templates: [
       {
         areas: ['a', 'b'],
-        breakpoint: Layout.getBreakpoint('xs'),
         behavior: 'up',
+        breakpoint: Layout.getBreakpoint('xs'),
       },
       {
         areas: ['a', 'b', 'c'],
-        breakpoint: Layout.getBreakpoint('md'),
         behavior: 'up',
+        breakpoint: Layout.getBreakpoint('md'),
+      },
+    ],
+  })
+})
+
+test('Returns proper areas list for "behavior:down"', () => {
+  const areasList = getAreasList({
+    template: `a b`,
+    templateMdDown: `c`,
+  })
+
+  expect(areasList).to.deep.equal({
+    areas: ['a', 'b', 'c'],
+    templates: [
+      {
+        areas: ['a', 'b'],
+        behavior: 'up',
+        breakpoint: Layout.getBreakpoint('xs'),
+      },
+      {
+        areas: ['c'],
+        behavior: 'down',
+        breakpoint: Layout.getBreakpoint('md'),
+      },
+    ],
+  })
+})
+
+test('Returns proper areas list for "behavior:only"', () => {
+  const areasList = getAreasList({
+    template: `a`,
+    templateMdOnly: `b c`,
+  })
+
+  expect(areasList).to.deep.equal({
+    areas: ['a', 'b', 'c'],
+    templates: [
+      {
+        areas: ['a'],
+        behavior: 'up',
+        breakpoint: Layout.getBreakpoint('xs'),
+      },
+      {
+        areas: ['b', 'c'],
+        behavior: 'only',
+        breakpoint: Layout.getBreakpoint('md'),
       },
     ],
   })
