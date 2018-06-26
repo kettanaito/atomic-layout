f21c64a375086d58bdf536661fa5c0d5fdbf8424
Artem Zakharchenko <kettanaito@gmail.com>
2018-06-26 23:01:47 +0200

    Updates Storybook port. Removes "examples" stories

--- stories/index.js
+++ stories/index.js
@@ -5,16 +5,6 @@ import './styles.css'
 import Playground from './Playground'
 import Demo from './Demo'
 
-/* Behavior */
-import MobileFirst from '../examples/behavior/MobileFirst'
-import Inclusive from '../examples/behavior/Inclusive'
-import Bell from '../examples/behavior/Bell'
-
 storiesOf('Stories', module)
   .add('Playground', () => <Playground />)
   .add('Demo', () => <Demo />)
-
-storiesOf('Behavior', module)
-  .add('Mobile first', () => <MobileFirst />)
-  .add('Inclusive', () => <Inclusive />)
-  .add('Bell', () => <Bell />)
