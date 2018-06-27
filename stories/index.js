7ccdbb8b2528b339a06ef311b0186f60ec29e722
Artem Zakharchenko <kettanaito@gmail.com>
2018-06-27 13:02:04 +0200

    Removes unused story

--- stories/index.js
+++ stories/index.js
@@ -4,9 +4,7 @@ import './styles.css'
 
 import Playground from './Playground'
 import Demo from './Demo'
-import Retina from './Retina'
 
 storiesOf('Stories', module)
   .add('Playground', () => <Playground />)
   .add('Demo', () => <Demo />)
-  .add('Retina', () => <Retina />)
