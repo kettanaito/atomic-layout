59db0c825da52ac4f6ea514867599d680eac4f30
Artem Zakharchenko <kettanaito@gmail.com>
2018-06-30 12:03:38 +0200

    getAreaParams: Explicitly appends "null" for templates that do not include the area name

--- src/utils/getAreaParams/getAreaParams.spec.js
+++ src/utils/getAreaParams/getAreaParams.spec.js
@@ -20,15 +20,17 @@ test('Mobile first', () => {
 })
 
 test('Inclusive', () => {
+  const breakpointMd = Layout.getBreakpoint('md')
+  const breakpointXl = Layout.getBreakpoint('xl')
+
   const { templates } = getAreasList({
     template: `'a'`,
     templateMd: `'a b'`,
     templateXl: `'a'`,
   })
+
   const areaParams = getAreaParams('b', templates)
 
-  const breakpointMd = Layout.getBreakpoint('md')
-  const breakpointXl = Layout.getBreakpoint('xl')
   expect(areaParams).to.deep.equal([
     null,
     {
@@ -36,30 +38,76 @@ test('Inclusive', () => {
       minWidth: breakpointMd.minWidth,
       maxWidth: breakpointXl.minWidth - 1,
     },
+    null,
   ])
 })
 
 test('Bell', () => {
+  const breakpointXs = Layout.getBreakpoint('xs')
+  const breakpointMd = Layout.getBreakpoint('md')
+  const breakpointXl = Layout.getBreakpoint('xl')
+
   const { templates } = getAreasList({
     template: `'a b'`,
     templateMd: `'a'`,
     templateXl: `'a b'`,
   })
+
   const areaParams = getAreaParams('b', templates)
 
-  const breakpointXs = Layout.getBreakpoint('xs')
-  const breakpointMd = Layout.getBreakpoint('md')
-  const breakpointXl = Layout.getBreakpoint('xl')
   expect(areaParams).to.deep.equal([
     {
       behavior: 'down',
       minWidth: breakpointXs.minWidth,
       maxWidth: breakpointMd.minWidth - 1,
     },
+    null,
     {
       behavior: 'up',
       minWidth: breakpointXl.minWidth,
       maxWidth: breakpointXl.maxWidth,
     },
   ])
+})
+
+describe('Shuffled behavior', () => {
+  test('Concatenates sibling areas with "down" behavior', () => {
+    const breakpointXs = Layout.getBreakpoint('xs')
+    const breakpointSm = Layout.getBreakpoint('sm')
+    const breakpointMd = Layout.getBreakpoint('md')
+
+    const { templates } = getAreasList({
+      template: `'a'`,
+      templateSmDown: `'c'`,
+      templateMdDown: 'a c',
+    })
+
+    /* Area "a" */
+    const areaParamsA = getAreaParams('a', templates)
+
+    expect(areaParamsA).to.deep.equal([
+      {
+        behavior: 'down',
+        minWidth: undefined,
+        maxWidth: breakpointXs.maxWidth,
+      },
+      null,
+      {
+        behavior: 'down',
+        minWidth: breakpointMd.minWidth,
+        maxWidth: breakpointMd.maxWidth,
+      },
+    ])
+
+    /* Area "c" */
+    const areaParamsC = getAreaParams('c', templates)
+    expect(areaParamsC).to.deep.equal([
+      null,
+      {
+        behavior: 'down',
+        minWidth: breakpointSm.minWidth,
+        maxWidth: breakpointMd.maxWidth,
+      },
+    ])
+  })
 })
