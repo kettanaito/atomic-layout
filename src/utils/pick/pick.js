b8cb93e17bfd76d04d9440932801cc5f3501fc2a
Matthias Margot <mmargot@airtouchmedia.com>
2018-06-30 14:57:01 +0300

    Optimize 'pick()' for speed semantics and readability
    - Iterate Object.keys twice with 1 only one computation each time (1st time over all the keys and 2nd time only over the keys that passed 'testPredicate()') instead of iterating once over everything with both computations in the same iteration
    
    - This is a two step operation, find the keys that exist -> populate the new object with the same values for those keys, splitting these into 2 iterations makes for better semantics of what's going on
    
    - less cluttered and removes the need for a ternary in the reducer function body
    
    - Syntax changes: renamed condition to comparator because well the condition is actually the whole testPredicate return value, removed return statement from returned function because it's a single value that's returned without any extra computation before the return, wrapper it in braces however because it's multi-line, did the same to 'testPredicate()' return value

--- src/utils/pick/pick.js
+++ src/utils/pick/pick.js
@@ -1,19 +1,21 @@
 // @flow
 export default function pick(predicate: Array<RegExp | string>) {
-  const testPredicate = (propName) => {
-    return predicate.some(
-      (condition) =>
-        typeof condition === 'string'
-          ? condition === propName
-          : condition.test(propName),
+  const testPredicate = (propName) =>
+    predicate.some(
+      (comparator) =>
+        typeof comparator === 'string'
+          ? comparator === propName
+          : comparator.test(propName),
     )
-  }
 
-  return (obj: Object): Object => {
-    return Object.keys(obj).reduce((picked: Object, propName) => {
-      return testPredicate(propName)
-        ? { ...picked, [propName]: obj[propName] }
-        : picked
-    }, {})
-  }
+  return (obj: Object): Object =>
+    Object.keys(obj)
+      .filter(testPredicate)
+      .reduce(
+        (picked: Object, propName) => ({
+          ...picked,
+          [propName]: obj[propName],
+        }),
+        {},
+      )
 }
