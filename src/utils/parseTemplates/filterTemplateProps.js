70c7064c29096c984c8bbd4c5091bfc72ec70a37
Artem Zakharchenko <kettanaito@gmail.com>
2018-07-07 18:50:56 +0200

    filterTemplateProps: Explicitly matches against "template" string followed by a breakpoint name

---
+++ src/utils/parseTemplates/filterTemplateProps.js
@@ -0,0 +1,18 @@
+// @flow
+import Layout from '../../Layout'
+import capitalize from '../strings/capitalize'
+import pick from '../pick'
+
+/**
+ * Filters properties of the given object to contain only those
+ * that are template declarations (match regular expression below).
+ */
+const filterTemplateProps = pick([
+  new RegExp(
+    `^template(${Layout.getBreakpointsNames()
+      .map(capitalize)
+      .join('|')})*$`,
+  ),
+])
+
+export default filterTemplateProps
