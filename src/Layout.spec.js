b68e3dcb1d1740922877854fdbe001d24c481ec7
Artem Zakharchenko <kettanaito@gmail.com>
2018-07-13 14:39:29 +0200

    Increases test coverage of "Layout" and "transformNumeric" functions

--- src/Layout.spec.js
+++ src/Layout.spec.js
@@ -2,12 +2,12 @@ import { expect } from 'chai'
 import defaultOptions from './const/defaultOptions'
 import Layout from './Layout'
 
-test('default options', () => {
+test('Sets default options properly', () => {
   expect(Layout).to.have.property('defaultUnit', defaultOptions.defaultUnit)
   expect(Layout).to.have.property('breakpoints', defaultOptions.breakpoints)
 })
 
-test('custom options', () => {
+test('Sets custom options properly', () => {
   expect(Layout)
     .to.have.property('configure')
     .to.be.a('function')
@@ -17,4 +17,24 @@ test('custom options', () => {
   })
 
   expect(Layout).to.have.property('defaultUnit', 'rem')
+})
+
+test('Returns breakpoints names', () => {
+  expect(Layout.getBreakpointsNames()).to.deep.equal([
+    'xs',
+    'sm',
+    'md',
+    'lg',
+    'xl',
+  ])
+})
+
+test('Returns existing breakpoint info', () => {
+  expect(Layout.getBreakpoint('md')).to.deep.equal(
+    defaultOptions.breakpoints.md,
+  )
+})
+
+test('Returns "undefined" for non-existing breakpoint', () => {
+  expect(Layout.getBreakpoint('foo')).to.equal(undefined)
 })
