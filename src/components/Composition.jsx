8efcb7995ed966bc5493bd34669e303849ed7f13
Artem Zakharchenko <kettanaito@gmail.com>
2018-08-13 14:52:20 +0200

    Composition: Handles invalid "areaComponents" object

--- src/components/Composition.jsx
+++ src/components/Composition.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import Box from './Box'
 import parseTemplates from '../utils/templates/parseTemplates'
+import invariant from '../utils/invariant'
 
 const CompositionWrapper = styled(Box)`
   display: ${({ inline }) => (inline ? 'inline-grid' : 'grid')};
@@ -16,9 +17,17 @@ export default class Composition extends React.Component {
   }
 
   render() {
+    const { areaComponents } = this
+    const hasAreaComponents = Object.keys(areaComponents).length > 0
+
+    invariant(
+      hasAreaComponents,
+      'Failed to render Composition: received no layout areas. Please check the values of template definitions.',
+    )
+
     return (
       <CompositionWrapper {...this.props}>
-        {this.props.children(this.areaComponents)}
+        {hasAreaComponents && this.props.children(areaComponents)}
       </CompositionWrapper>
     )
   }
