0aa598808bb9fd46693caf5ad239f5fca7b6b330
Artem Zakharchenko <kettanaito@gmail.com>
2018-09-21 13:18:22 +0200

    Updates to "babel@7"

---
+++ examples/babel.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-react',
+  ],
+  plugins: [
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-proposal-export-default-from',
+    '@babel/plugin-proposal-export-namespace-from',
+  ]
+}
