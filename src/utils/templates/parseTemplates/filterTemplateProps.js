fc085da409b804fa9c0fbe7c3d138d676892af1c
Artem Zakharchenko <kettanaito@gmail.com>
2018-09-28 13:07:56 +0200

    filterTemplateProps: Uses functional composition

--- src/utils/templates/parseTemplates/filterTemplateProps.js
+++ src/utils/templates/parseTemplates/filterTemplateProps.js
@@ -1,23 +1,36 @@
 // @flow
+import * as R from 'ramda'
 import parsePropName from '../../strings/parsePropName'
+
+/**
+ * Returns the derived props that contain only template
+ * declarations.
+ */
+const filterTemplateProps = R.pickBy(
+  R.compose(
+    R.propEq('purePropName', 'template'),
+    parsePropName,
+    R.nthArg(1),
+  ),
+)
 
 /**
  * Filters the given object to contain only templates
  * declarations (props matching the RegExp).
  */
-const filterTemplateProps = (obj: Object) => {
-  return Object.keys(obj)
-    .filter((propName) => {
-      const { purePropName } = parsePropName(propName)
-      return purePropName === 'template'
-    })
-    .reduce(
-      (acc, propName) => ({
-        ...acc,
-        [propName]: obj[propName],
-      }),
-      {},
-    )
-}
+// const filterTemplateProps = (obj: Object) => {
+//   return Object.keys(obj)
+//     .filter((propName) => {
+//       const { purePropName } = parsePropName(propName)
+//       return purePropName === 'template'
+//     })
+//     .reduce(
+//       (acc, propName) => ({
+//         ...acc,
+//         [propName]: obj[propName],
+//       }),
+//       {},
+//     )
+// }
 
 export default filterTemplateProps
