a24867c89cb71394676ec67bdbc9dabba0687a52
Artem Zakharchenko <kettanaito@gmail.com>
2018-10-06 16:29:22 +0200

    Box: Acquires flexbox display by default

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -2,7 +2,8 @@ import styled from 'styled-components'
 import applyStyles from '../utils/styles/applyStyles'
 
 const Box = styled.div`
-  ${(props) => applyStyles(props)};
+  display: ${({ inline }) => (inline ? 'inline-flex' : 'flex')};
+  ${applyStyles};
 `
 
 export default Box
