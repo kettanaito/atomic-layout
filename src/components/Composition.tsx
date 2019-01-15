4038014177457a1d2dff41867fa754dc3fb83f61
Artem Zakharchenko <kettanaito@gmail.com>
2019-01-15 11:37:29 +0100

    Imports named exports as "React"

--- src/components/Composition.tsx
+++ src/components/Composition.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import * as React from 'react'
 import styled from 'styled-components'
 import { GenericProps, GridProps } from '../const/props'
 import { AreasMap } from '../utils/templates/generateComponents'
--- src/components/Only.tsx
+++ src/components/Only.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import * as React from 'react'
 import MediaQuery from 'react-responsive'
 import { Breakpoint } from '../const/defaultOptions'
 import { GenericProps } from '../const/props'
