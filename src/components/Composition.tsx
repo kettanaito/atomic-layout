edb3978245d05f8afabdb48aad9d029f7f751a90
Artem Zakharchenko <kettanaito@gmail.com>
2019-01-16 09:45:40 +0100

    Composition: Warn on template-full render without children-as-function

--- src/components/Composition.tsx
+++ src/components/Composition.tsx
@@ -4,11 +4,11 @@ import { GenericProps, GridProps } from '../const/props'
 import { AreasMap } from '../utils/templates/generateComponents'
 import parseTemplates from '../utils/templates/parseTemplates'
 import applyStyles from '../utils/styles/applyStyles'
+import invariant from '../utils/invariant'
 
 type ChildrenFunction = (areas: AreasMap) => React.ReactNode
 
 interface CompositionProps extends GenericProps, GridProps {
-  [propName: string]: any
   children: ChildrenFunction | React.ReactNode
   inline?: boolean
 }
@@ -24,10 +24,22 @@ const Composition: React.FunctionComponent<CompositionProps> = ({
 }) => {
   const areaComponents = parseTemplates(restProps)
   const hasAreaComponents = Object.keys(areaComponents).length > 0
+  const childrenType = typeof children
+  const hasChildrenFunction = childrenType === 'function'
+
+  /**
+   * Warn on attempt to use template props without children-as-function.
+   */
+  invariant(
+    !(hasAreaComponents && !hasChildrenFunction),
+    'Failed to render `Composition` with template areas ["%s"]: expected children to be a function, but got: %s. Please remove template props, or add a children rendering function.',
+    Object.keys(areaComponents).join('", "'),
+    childrenType,
+  )
 
   return (
     <CompositionWrapper {...restProps}>
-      {hasAreaComponents
+      {hasAreaComponents && hasChildrenFunction
         ? (children as ChildrenFunction)(areaComponents)
         : children}
     </CompositionWrapper>
