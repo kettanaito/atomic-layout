5c160a551c1dad2b29cca4da10a496f0f5063815
ruhaise <ruhaise@gmail.com>
2019-02-13 16:25:26 +0100

    extra check not to add suffix if the value is zero

--- src/utils/math/transformNumeric.spec.ts
+++ src/utils/math/transformNumeric.spec.ts
@@ -4,7 +4,7 @@ test('Suffixes numeric values with "px" string', () => {
   expect(transformNumeric(5)).toEqual('5px')
 })
 
-test('Returns string values as is', () => {
+test('Returns string values as it is', () => {
   expect(transformNumeric('2vh')).toEqual('2vh')
 })
 
@@ -12,12 +12,7 @@ test('Returns empty string for empty string as value', () => {
   expect(transformNumeric('')).toEqual('')
 })
 
-test('Handles explicit 0 as a value', () => {
+test('Handles explicit 0 as a value and no suffix is attached', () => {
   expect(transformNumeric('0')).toEqual('0')
-  expect(transformNumeric(0)).toEqual('0px')
-})
-
-test('Outputs CSS for 0 as a value', () => {
-  expect(transformNumeric('0')).toEqual('0')
-  expect(transformNumeric(0)).toEqual('0px')
+  expect(transformNumeric(0)).toEqual('0')
 })
