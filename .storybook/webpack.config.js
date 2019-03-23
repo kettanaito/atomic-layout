5e7c989c5f83f643f936845aa7f1d5c52b785cd7
Artem Zakharchenko <kettanaito@gmail.com>
2019-03-23 17:23:07 +0100

    Puts stories into "examples" directory

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = async ({ config }) => {
   config.resolve.alias = {
     'atomic-layout': path.resolve(__dirname, '../lib'),
-    '@stories': path.resolve(__dirname, '../stories'),
+    '@stories': path.resolve(__dirname, '../examples'),
   }
   return config
 }
