9b8965d4e2fcccfa60485e3d927a3c75e35d7796
kettanaito <kettanaito@gmail.com>
2019-04-13 17:52:02 +0200

    Exports "withBreakpoints" util through public API

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,13 @@ import Layout from './Layout'
 export default Layout
 
 /* Components */
-export { default as Box } from './components/Box'
-export { default as Composition } from './components/Composition'
-export { default as Only } from './components/Only'
-export { default as MediaQuery } from './components/MediaQuery'
+export { default as Box } from '@components/Box'
+export { default as Composition } from '@components/Composition'
+export { default as Only } from '@components/Only'
+export { default as MediaQuery } from '@components/MediaQuery'
 
 /* Hooks */
 export { default as useResponsiveValue } from './hooks/useResponsiveValue'
+
+/* Utils */
+export { default as withBreakpoints } from '@utils/breakpoints/withBreakpoints'
