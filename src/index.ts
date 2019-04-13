59e85daeb1c23f27647b0a9f9473f08fbe1b00a5
kettanaito <kettanaito@gmail.com>
2019-04-13 10:26:14 +0200

    Removes leftovers

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,3 @@ export { default as MediaQuery } from './components/MediaQuery'
 export { default as Box } from './components/Box'
 export { default as Composition } from './components/Composition'
 export { default as Only } from './components/Only'
-
-console.log('f')
