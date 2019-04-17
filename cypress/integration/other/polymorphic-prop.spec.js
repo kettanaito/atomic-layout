a28c9b7ea0ed25f7684c6c2f6e84e5d3c733a5ef
kettanaito <kettanaito@gmail.com>
2019-04-17 19:58:41 +0200

    Adds integration test for styles overrides

--- cypress/integration/other/polymorphic-prop.spec.js
+++ cypress/integration/other/polymorphic-prop.spec.js
@@ -8,8 +8,27 @@ describe('Polymorphic prop', () => {
   })
 
   it('Renders polymorphic prop', () => {
-    cy.get('#header').haveTag('header')
     cy.get('#main').haveTag('main')
     cy.get('#footer').haveTag('footer')
+  })
+
+  describe('When using custom React component', () => {
+    it('renders custom React component', () => {
+      cy.get('#header').haveTag('header')
+    })
+
+    it('preserves irrelevant styles', () => {
+      cy.get('#header').should(
+        'have.css',
+        'background-color',
+        'rgb(238, 238, 238)',
+      )
+    })
+
+    it('overrides layout styles', () => {
+      cy.get('#header')
+        .should('have.css', 'display', 'flex')
+        .should('have.css', 'padding', '20px')
+    })
   })
 })
