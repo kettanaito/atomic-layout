412dd50409a8badd25e70bb0df414fa096ef1904
kettanaito <kettanaito@gmail.com>
2019-04-17 19:58:41 +0200

    Adds integration test for styles overrides

--- examples/Core/Rendering/PolymorphicProp.jsx
+++ examples/Core/Rendering/PolymorphicProp.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import styled from 'styled-components'
 import { Composition } from 'atomic-layout'
 import Square from '@stories/Square'
+
+const CustomHeader = styled.header`
+  background-color: #eee;
+  display: block;
+  padding: 10px;
+`
 
 const PolymorphicProp = () => (
   <Composition areas="header main element footer" gutter={10}>
     {({ Header, Main, Element, Footer }) => (
       <React.Fragment>
-        <Header as="header" id="header">
+        <Header flex as={CustomHeader} id="header" padding={20}>
           <Square>Header</Square>
         </Header>
         <Main as="main" id="main">
