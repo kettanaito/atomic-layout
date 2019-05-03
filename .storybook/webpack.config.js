936bc9b6c84287ee5fb3a39339df08b39c3db2e8
kettanaito <kettanaito@gmail.com>
2019-05-03 11:45:24 +0200

    Installs rollup packages

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = async ({ config }) => {
   config.resolve.alias = {
-    'atomic-layout': path.resolve(__dirname, '../lib'),
+    'atomic-layout': path.resolve(__dirname, '../lib/atomic-layout.cjs.js'),
     '@stories': path.resolve(__dirname, '../examples'),
   }
   return config
