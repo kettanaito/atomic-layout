9dee820d79eb24f1575d9f6bb5f939601974e9e7
kettanaito <kettanaito@gmail.com>
2019-06-02 20:35:34 +0300

    useViewportChange: sets default debounce duration to 50ms

--- src/hooks/useViewportChange.ts
+++ src/hooks/useViewportChange.ts
@@ -4,7 +4,10 @@ import debounce from '@utils/functions/debounce'
 /**
  * Executes a given callback on debounced window resize.
  */
-const useViewportChange = (callback: () => void, debounceDuration?: number) => {
+const useViewportChange = (
+  callback: () => void,
+  debounceDuration: number = 50,
+) => {
   const handleWindowResize = debounce(callback, debounceDuration)
 
   useEffect(() => {
