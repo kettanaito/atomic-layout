96be65bbcb1ab49e7825c6d7470d4c7cd03f7b07
kettanaito <kettanaito@gmail.com>
2019-06-29 12:03:04 +0200

    Configures multi-target build with rollup

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = async ({ config }) => {
   config.resolve.alias = {
-    'atomic-layout': path.resolve(__dirname, '../lib/atomic-layout.cjs.js'),
+    'atomic-layout': path.resolve(__dirname, '../lib/cjs.js'),
     '@stories': path.resolve(__dirname, '../examples'),
   }
   return config
