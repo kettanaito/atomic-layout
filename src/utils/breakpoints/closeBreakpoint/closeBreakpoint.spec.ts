a3c14adc903beb20362ab77cf584c4b913dfd463
Artem Zakharchenko <kettanaito@gmail.com>
2019-07-10 08:44:14 +0200

    closeBreakpoint: preserves original measurement unit of a prop value

--- src/utils/breakpoints/closeBreakpoint/closeBreakpoint.spec.ts
+++ src/utils/breakpoints/closeBreakpoint/closeBreakpoint.spec.ts
@@ -1,17 +1,39 @@
 import closeBreakpoint from './closeBreakpoint'
 
 describe('closeBreakpoint', () => {
-  it('closes a given breakpoint', () => {
-    const breakpoint = {
-      minWidth: 500,
-      maxWidth: 621,
+  describe('when given numeric breakpoints', () => {
+    const breakpoint = closeBreakpoint({
       minHeight: 350,
       maxHeight: 700,
-    }
+      minWidth: 500,
+      maxWidth: 621,
+    })
 
-    expect(closeBreakpoint(breakpoint)).toEqual({
-      maxHeight: 349,
-      maxWidth: 499,
+    it('should not contain original "min-" properties', () => {
+      expect(breakpoint).not.toContain(['minHeight', 'minWidth'])
+    })
+
+    it('should coerce "max-" properties into {min} - 1', () => {
+      expect(breakpoint).toHaveProperty('maxHeight', 349)
+      expect(breakpoint).toHaveProperty('maxWidth', 499)
+    })
+  })
+
+  describe('when using custom measurement unit', () => {
+    const breakpoint = closeBreakpoint({
+      minHeight: '350px',
+      maxHeight: '700px',
+      minWidth: '500px',
+      maxWidth: '621px',
+    })
+
+    it('should not contain original "min-" properties', () => {
+      expect(breakpoint).not.toContain(['minHeight', 'minWidth'])
+    })
+
+    it('should coerce "max-" properties into {min} - 1 + unit', () => {
+      expect(breakpoint).toHaveProperty('maxHeight', '349px')
+      expect(breakpoint).toHaveProperty('maxWidth', '499px')
     })
   })
 })
