11d48cf0f47f24e157fa02153158f713be932c86
Artem Zakharchenko <kettanaito@gmail.com>
2019-07-12 16:10:39 +0200

    Logs out bundled module version during Storybook build

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -3,6 +3,8 @@ const path = require('path')
 module.exports = async ({ config }) => {
   const target = process.env.TARGET || 'cjs'
 
+  console.log(`\nBundling "atomic-layout" build for "${target}" module...\n`)
+
   config.resolve.alias = {
     'atomic-layout': path.resolve(__dirname, `../${target}`),
     '@stories': path.resolve(__dirname, '../examples'),
