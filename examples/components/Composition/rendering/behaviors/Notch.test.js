3d4422c08539731677aa46620d745cf5a740a3b3
Artem Zakharchenko <kettanaito@gmail.com>
2019-08-25 11:36:11 +0200

    Separates reused Bell and Notch behavior assertions into custom Cypress commands

--- examples/components/Composition/rendering/behaviors/Notch.test.js
+++ examples/components/Composition/rendering/behaviors/Notch.test.js
@@ -1,22 +1,8 @@
-export const assertNotch = () => {
-  const assertAllVisible = () => {
-    cy.get('#notch').assertAreas([['left', 'center', 'right']])
-  }
-
-  const assertCenterHidden = () => {
-    cy.get('#notch').assertAreas([['left', 'right']])
-  }
-
-  assertAllVisible()
-  cy.setBreakpoint('sm').then(assertAllVisible)
-  cy.setBreakpoint('md').then(assertCenterHidden)
-  cy.setBreakpoint('lg').then(assertAllVisible)
-  cy.setBreakpoint('xl').then(assertAllVisible)
-}
-
 it('Notch', () => {
   cy.loadStory(
     ['components', 'composition', 'rendering', 'behaviors'],
     ['notch'],
   )
+
+  cy.get('#notch').assertNotch()
 })
