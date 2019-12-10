dfbe81608349ffc1d3f809ae5c1ace06f40e8740
Artem Zakharchenko <kettanaito@gmail.com>
2019-12-10 19:44:46 +0100

    Configures UMD build for "@atomic-layout/emotion"

---
+++ packages/atomic-layout-emotion/babel.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        modules: false,
+      },
+    ],
+    '@babel/preset-react',
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-proposal-export-default-from',
+    '@babel/plugin-proposal-export-namespace-from',
+  ],
+  env: {
+    test: {
+      presets: ['@babel/preset-env', '@babel/preset-react'],
+    },
+  },
+}
