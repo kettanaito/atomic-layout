7d76530ec76a2a027f0eaaed4561f3106c36d9ea
Artem Zakharchenko <kettanaito@gmail.com>
2019-12-11 09:27:13 +0100

    Moves "babel.config.js" to the root-level of the monorepo

--- packages/atomic-layout-emotion/babel.config.js
+++ packages/atomic-layout-emotion/babel.config.js
@@ -3,5 +3,5 @@ const path = require('path')
 module.exports = {
   // Resolve the path otherwise it gets resolved relatively
   // to Storybook main entry module during the Storybook build.
-  extends: path.resolve(__dirname, '..', 'atomic-layout/babel.config.js'),
+  extends: path.resolve(__dirname, '../..', 'babel.config.js'),
 }
