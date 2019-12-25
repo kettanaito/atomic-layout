3fd0899c2ec323dc52716c538d70922ce820bab5
Artem Zakharchenko <kettanaito@gmail.com>
2019-12-25 12:04:08 +0100

    useResponsiveComponent: Uses "React.forwardRef" to propagate ref

--- packages/atomic-layout/examples/hooks/UseResponsiveComponent.jsx
+++ packages/atomic-layout/examples/hooks/UseResponsiveComponent.jsx
@@ -9,18 +9,33 @@ const Text = useResponsiveComponent(
   `,
 )
 
-const Scenario = () => (
-  <Text
-    id="text"
-    size={16}
-    sizeMd={20}
-    sizeLg={24}
-    color="black"
-    colorSm="green"
-    colorLg="red"
-  >
-    Quick brown fox
-  </Text>
-)
+const Scenario = () => {
+  const textRef = React.useRef({})
+  const [refDerivedData, setRefDerivedData] = React.useState(null)
+
+  React.useEffect(() => {
+    setRefDerivedData(textRef.current.id)
+  }, [textRef.current])
+
+  return (
+    <div>
+      <Text
+        ref={textRef}
+        id="text"
+        size={16}
+        sizeMd={20}
+        sizeLg={24}
+        color="black"
+        colorSm="green"
+        colorLg="red"
+      >
+        Quick brown fox
+      </Text>
+      <p>
+        Ref: <span id="ref">#{refDerivedData}</span>
+      </p>
+    </div>
+  )
+}
 
 export default Scenario
--- packages/atomic-layout/examples/hooks/useResponsiveComponent.test.js
+++ packages/atomic-layout/examples/hooks/useResponsiveComponent.test.js
@@ -3,35 +3,43 @@ describe('useResponsiveComponent', () => {
     cy.loadStory(['hooks'], ['useresponsivecomponent'])
   })
 
-  it('custom responsive prop has correct initial state', () => {
-    cy.get('#text')
-      .should('have.css', 'font-size', '16px')
-      .should('have.css', 'color', 'rgb(0, 0, 0)')
-  })
-
-  it('custom responsive prop changes correctly on breakpoint changes', () => {
-    cy.setBreakpoint('sm').then(() => {
+  describe('Prmiary behavior', () => {
+    it('custom responsive prop has correct initial state', () => {
       cy.get('#text')
         .should('have.css', 'font-size', '16px')
-        .should('have.css', 'color', 'rgb(0, 128, 0)')
+        .should('have.css', 'color', 'rgb(0, 0, 0)')
     })
 
-    cy.setBreakpoint('md').then(() => {
-      cy.get('#text')
-        .should('have.css', 'font-size', '20px')
-        .should('have.css', 'color', 'rgb(0, 128, 0)')
-    })
+    it('custom responsive prop changes correctly on breakpoint changes', () => {
+      cy.setBreakpoint('sm').then(() => {
+        cy.get('#text')
+          .should('have.css', 'font-size', '16px')
+          .should('have.css', 'color', 'rgb(0, 128, 0)')
+      })
 
-    cy.setBreakpoint('lg').then(() => {
-      cy.get('#text')
-        .should('have.css', 'font-size', '24px')
-        .should('have.css', 'color', 'rgb(255, 0, 0)')
-    })
+      cy.setBreakpoint('md').then(() => {
+        cy.get('#text')
+          .should('have.css', 'font-size', '20px')
+          .should('have.css', 'color', 'rgb(0, 128, 0)')
+      })
 
-    cy.setBreakpoint('xl').then(() => {
-      cy.get('#text')
-        .should('have.css', 'font-size', '24px')
-        .should('have.css', 'color', 'rgb(255, 0, 0)')
+      cy.setBreakpoint('lg').then(() => {
+        cy.get('#text')
+          .should('have.css', 'font-size', '24px')
+          .should('have.css', 'color', 'rgb(255, 0, 0)')
+      })
+
+      cy.setBreakpoint('xl').then(() => {
+        cy.get('#text')
+          .should('have.css', 'font-size', '24px')
+          .should('have.css', 'color', 'rgb(255, 0, 0)')
+      })
+    })
+  })
+
+  describe('Generic behavior', () => {
+    it('supports ref forwarding', () => {
+      cy.get('#ref').should('have.text', '#text')
     })
   })
 })
