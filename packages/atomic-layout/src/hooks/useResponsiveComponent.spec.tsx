db1835149f5260a9fa91a629a55494e2bc8b9a61
Artem Zakharchenko <kettanaito@gmail.com>
2020-01-02 15:08:32 +0100

    useResponsiveProps: Fixes default breakpoint props assignment on SSR

---
+++ packages/atomic-layout/src/hooks/useResponsiveComponent.spec.tsx
@@ -0,0 +1,34 @@
+/**
+ * @jest-environment node
+ */
+import React from 'react'
+import { renderToString } from 'react-dom/server'
+import useResponsiveComponent from './useResponsiveComponent'
+
+const Component = useResponsiveComponent((props) => {
+  return <img {...props} />
+})
+
+describe('useResponsiveComponent', () => {
+  describe('given rendered on a server', () => {
+    let html: ReturnType<typeof renderToString>
+
+    beforeAll(() => {
+      html = renderToString(
+        <Component src="image.png" altMd="Image" titleLgDown="Title" />,
+      )
+    })
+
+    it('should have responsive prop with default breakpoint', () => {
+      expect(html).toContain('src="image.png"')
+    })
+
+    it.skip('should have responsive prop with "down" behavior', () => {
+      expect(html).toContain('title="Title"')
+    })
+
+    it('should not have any responsive prop with other breakpoints', () => {
+      expect(html).not.toContain('alt')
+    })
+  })
+})
