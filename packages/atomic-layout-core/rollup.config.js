5df61745a43a6b583988540e36123dbf1fb0a965
Artem Zakharchenko <kettanaito@gmail.com>
2020-01-20 14:44:33 +0100

    @atomic-layout/core: Stops publishing the source directory

--- packages/atomic-layout-core/rollup.config.js
+++ packages/atomic-layout-core/rollup.config.js
@@ -1,6 +1,7 @@
 import path from 'path'
 import replace from 'rollup-plugin-replace'
 import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
 import typescript from 'rollup-plugin-typescript2'
 import packageJson from './package.json'
 
@@ -9,10 +10,11 @@ const PRODUCTION = nodeEnv === 'production'
 const __PROD__ = PRODUCTION ? 'true' : '""'
 
 export default {
-  input: packageJson.esnext,
+  input: 'src/index.ts',
   output: {
     file: path.resolve(__dirname, packageJson.main),
     format: 'cjs',
+    exports: 'named',
   },
   plugins: [
     resolve({
@@ -20,6 +22,7 @@ export default {
       extensions: ['.ts'],
     }),
     typescript(),
+    commonjs(),
     replace({
       __PROD__,
     }),
