d521e7ff39fbdaafe3c9e757d9dd03719b730785
Artem Zakharchenko <kettanaito@gmail.com>
2020-01-23 14:47:04 +0100

    @atomic-layout/emotion: Replaces imports to "styled-components" with "@emotion/styled"

--- packages/atomic-layout-emotion/babel.config.js
+++ packages/atomic-layout-emotion/babel.config.js
@@ -11,4 +11,15 @@ module.exports = {
   // Resolve the path otherwise it gets resolved relatively
   // to Storybook main entry module during the Storybook build.
   extends: babelConfigPath,
+  plugins: [
+    [
+      'transform-rename-import',
+      {
+        // Replace imports to "styled-components" with imports to emotion.
+        // Applied to the modules imported from "atomic-layout" package.
+        original: '^styled-components',
+        replacement: '@emotion/styled',
+      },
+    ],
+  ],
 }
