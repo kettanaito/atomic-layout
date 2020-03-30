8c20d88f75df89f7022821e65678f7e123dd3ae8
Artem Zakharchenko <kettanaito@gmail.com>
2020-03-30 10:41:23 +0200

    Adds E2E tests for components "displayName"

---
+++ examples/regression/DisplayNames.test.js
@@ -0,0 +1,33 @@
+describe('Display names', () => {
+  before(() => {
+    cy.loadStory(['regression', 'all'], ['display-names'])
+  })
+
+  it('Box should have a meaningful display name', () => {
+    cy.get('#box').should('have.text', 'Box')
+  })
+
+  it('Composition should have meaningful display name', () => {
+    cy.get('#composition').should(
+      'have.attr',
+      'data-display-name',
+      'Composition',
+    )
+  })
+
+  it('Composition areas should have meaningful display name', () => {
+    cy.get('#composition-area').should('have.text', 'Area(One)')
+  })
+
+  it('Only should have meaningful display name', () => {
+    cy.get('#only').should('have.text', 'Only')
+  })
+
+  it('Visible should have meaningful display name', () => {
+    cy.get('#visible').should('have.text', 'Visible')
+  })
+
+  it('MediaQuery should have meaningful display name', () => {
+    cy.get('#media-query').should('have.text', 'MediaQuery')
+  })
+})
