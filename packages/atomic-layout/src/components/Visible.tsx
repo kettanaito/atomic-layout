1d643992abcf0ec6e0baa709a7e8bb1a06f2316d
Artem Zakharchenko <kettanaito@gmail.com>
2020-04-05 18:21:06 +0200

    Visible: Uses "useResponsiveQuery" hook for conditional display

--- packages/atomic-layout/src/components/Visible.tsx
+++ packages/atomic-layout/src/components/Visible.tsx
@@ -1,15 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
-import {
-  openBreakpoint,
-  closeBreakpoint,
-  mergeAreaRecords,
-} from '@atomic-layout/core'
-
 import Box from './Box'
 import { OnlyProps } from './Only'
-import { useMediaQuery } from '../hooks/useMediaQuery'
-import { resolveBreakpoint } from '../hooks/useResponsiveQuery'
+import useResponsiveQuery from '../hooks/useResponsiveQuery'
 
 const VisibleContainer = styled(Box)<{ matches: boolean }>`
   ${({ matches }) =>
@@ -19,57 +12,27 @@ const VisibleContainer = styled(Box)<{ matches: boolean }>`
     `}
 `
 
+/**
+ * Displays children when the given responsive query matches.
+ */
 const Visible: React.FC<OnlyProps> = ({
   children,
   except,
   for: exactBreakpointName,
-  from: minBreakpointName,
-  to: maxBreakpointName,
+  from,
+  to,
   ...boxProps
 }) => {
-  const exactBreakpoint = resolveBreakpoint(exactBreakpointName)
-  const minBreakpoint = resolveBreakpoint(minBreakpointName)
-  const maxBreakpoint = resolveBreakpoint(maxBreakpointName)
-
-  let mediaQuery = exactBreakpoint
-
-  // Bell, __/--\__
-  if (minBreakpoint && maxBreakpoint && !except) {
-    const { breakpoint } = mergeAreaRecords(
-      {
-        behavior: 'down',
-        breakpoint: maxBreakpoint,
-      },
-      {
-        behavior: 'up',
-        breakpoint: minBreakpoint,
-      },
-      false,
-    )
-
-    mediaQuery = breakpoint
-  }
-
-  // Notch, --\__/--
-  if (minBreakpoint && maxBreakpoint && except) {
-    mediaQuery = [closeBreakpoint(minBreakpoint), openBreakpoint(maxBreakpoint)]
-  }
-
-  // High-pass, __/--
-  if (minBreakpoint && !maxBreakpoint) {
-    mediaQuery = openBreakpoint(minBreakpoint)
-  }
-
-  // Low-pass, --\__
-  if (!minBreakpoint && maxBreakpoint) {
-    mediaQuery = closeBreakpoint(maxBreakpoint)
-  }
-
-  const matches = useMediaQuery(mediaQuery)
-  const ariaHidden = !matches ? { 'aria-hidden': 'true' } : {}
+  const matches = useResponsiveQuery({
+    except,
+    for: exactBreakpointName,
+    from,
+    to,
+  })
+  const ariaAttributes = !matches ? { 'aria-hidden': 'true' } : {}
 
   return (
-    <VisibleContainer {...boxProps} {...ariaHidden} matches={matches}>
+    <VisibleContainer {...boxProps} {...ariaAttributes} matches={matches}>
       {children}
     </VisibleContainer>
   )
