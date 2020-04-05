93c9a13b500cb2ee07da7ef073301e6403215d0b
Artem Zakharchenko <kettanaito@gmail.com>
2020-04-05 18:44:03 +0200

    @atomic-layout/core: Removes unused "when" function

--- packages/atomic-layout-core/src/utils/functions/when/when.ts
+++
@@ -1,6 +0,0 @@
-export default function when(
-  predicate: (...args: any[]) => boolean,
-  whenTrueFunc: (...args: any[]) => any,
-) {
-  return (args: any) => (predicate(args) ? whenTrueFunc(args) : args)
-}
